4095e7df7fd9895f0fecba21bd62c03fe20da515
Eugene Pashkevich <Eugene.Pashkevich@1pt.com>
2025-07-18 19:52:58 +0300

    init

---
+++ 01_Arrays.playground/Contents.swift
@@ -0,0 +1,72 @@
+
+import Foundation
+
+// 1. Посчитать сумму положительных чисел в массиве
+//[1,-4,7,12] -> 1 + 7 + 12 = 20
+
+//var arr = [1,-4,7,12];
+//
+//func sumOfPositives(_ array: [Int]) -> Int {
+//    var result = 0;
+//
+//    for item in array {
+//        if (item >= 0) {
+//            result += item
+//        }
+//    }
+//    
+//    return result;
+//}
+//
+//print(sumOfPositives(arr)); // 20
+
+// ========================================================= //
+
+// 2. Посчитать произведение элементов в массиве
+//[1, 2, 3, 4] -> 1 * 2 * 3 * 4 = 24
+//[0, 1, 2, 3, 4] -> 0 * 1 * 2 * 3 * 4 = 0
+
+//var arrayFirst = [1,2,3,4];
+//var arraySecond = [0,1,2,3,4];
+//
+//func multiplyOfElements(_ array: [Int]) -> Int {
+//    var result = 1;
+//    
+//    for item in array {
+//        result = result * item
+//    }
+//    
+//    return result;
+//}
+//
+//print(multiplyOfElements(arrayFirst)) // 24
+//print(multiplyOfElements(arraySecond)) // 0
+
+// ========================================================= //
+
+// 3. Возвести каждый элементв в квадрат
+// [1, 2, 3, 4, 5] -> [1, 4, 9, 16, 25]
+
+//var arr = [1, 2, 3, 4, 5];
+//
+//func squareElements(_ array: inout [Int]) -> [Int] {
+//    var result: [Int] = [];
+//    
+//    for item in array {
+//        result.append(item * item);
+//    }
+//    
+//    return result;
+//}
+//
+//print(squareElements(&arr));
+
+// ========================================================= //
+
+
+
+// ========================================================= //
+// ========================================================= //
+// ========================================================= //
+// ========================================================= //
+
